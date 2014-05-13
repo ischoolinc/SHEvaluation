0c64eb1d8a93082e018d0714bf41d97494c6f9b0
imcloudwu <imcloudwu@gmail.com>
2014-05-13 09:37:40 +0800

    報表權限調整

---
+++ SH/SH_SemesterScoreReport/SH_SemesterScoreReport/Permissions.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SH_SemesterScoreReport
+{
+    class Permissions
+    {
+        public static string 期末成績通知單 { get { return "SH_SemesterScoreReport.6D7169EF-5BAD-41C5-B999-0946F37A6F92"; } }
+
+        public static bool 期末成績通知單權限
+        {
+            get { return FISCA.Permission.UserAcl.Current[期末成績通知單].Executable; }
+        }
+    }
+}

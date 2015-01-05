60d9a0ce52f0b7eefce34ca82c90ccc93ec43036
chentaichen <ct.chen@ischool.com.tw>
2015-01-05 09:42:07 +0800

    版本編號

--- SmartSchool.Evaluation.HS/Properties/AssemblyInfo.cs
+++ SmartSchool.Evaluation.HS/Properties/AssemblyInfo.cs
@@ -29,5 +29,5 @@ using System.Runtime.InteropServices;
 //      組建編號
 //      修訂編號
 //
-[assembly: AssemblyVersion("1.0.6.8")]
-[assembly: AssemblyFileVersion("1.0.6.8")]
+[assembly: AssemblyVersion("1.0.6.9")]
+[assembly: AssemblyFileVersion("1.0.6.9")]

5a1f0195964eca8e71e0a6f345db07a392a35c52
CT Chen <ct.chen@ischool.com.tw>
2025-10-20 22:10:17 +0800

    調整產生學習歷程成績，當學籍異動，221、223，跨課綱復學，身分別由3改成4。

--- SmartSchool.Evaluation.HS/Properties/AssemblyInfo.cs
+++ SmartSchool.Evaluation.HS/Properties/AssemblyInfo.cs
@@ -29,5 +29,5 @@ using System.Runtime.InteropServices;
 //      修訂編號
 //
 
-[assembly: AssemblyVersion("1.0.7.127")]
-[assembly: AssemblyFileVersion("1.0.7.127")]
+[assembly: AssemblyVersion("1.0.7.128")]
+[assembly: AssemblyFileVersion("1.0.7.128")]

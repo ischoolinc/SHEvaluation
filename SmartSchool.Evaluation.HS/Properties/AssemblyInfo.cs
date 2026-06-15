22f6830c96908866112b996eaf7f30accbd2c013
CT Chen <ct.chen@ischool.com.tw>
2026-06-15 15:27:58 +0800

    調整重補修成績匯入表，當是否補修成績屬性 = 是，匯入表是否補修成績欄位填入"是"，其他或空值都填入"否"。

--- SmartSchool.Evaluation.HS/Properties/AssemblyInfo.cs
+++ SmartSchool.Evaluation.HS/Properties/AssemblyInfo.cs
@@ -29,5 +29,5 @@ using System.Runtime.InteropServices;
 //      修訂編號
 //
 
-[assembly: AssemblyVersion("1.0.7.148")]
-[assembly: AssemblyFileVersion("1.0.7.148")]
+[assembly: AssemblyVersion("1.0.7.149")]
+[assembly: AssemblyFileVersion("1.0.7.149")]
